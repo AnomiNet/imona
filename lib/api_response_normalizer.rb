aca2da2eb2bde46192e349f9314ae36af796b11d
Ayumi Yu <ayumi@ayumiyu.com>
2015-07-15 21:56:26 -0700

    Fix JSON response massage errors

--- lib/api_response_normalizer.rb
+++ lib/api_response_normalizer.rb
@@ -2,8 +2,8 @@ class ApiResponseNormalizer < Faraday::Response::Middleware
   def on_complete(env)
     if env[:body].blank?
       env[:body] = '{}'
-    elsif !( env[:body] =~ /^\{.*?\}$/ ) \
-    and !( env[:body] =~ /^\[.*?\]$/ )
+    elsif !( env[:body][0] == '{' and env[:body][-1] == '}' ) \
+    and !( env[:body][0] == '[' and env[:body][-1] == ']' )
       env[:body] = "{\"errors\":[\"#{ env[:body] }\"]}"
       Rails.logger.warn "Massaged malformed Api response to #{ env[:body] }"
     end
